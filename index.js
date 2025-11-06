6fcb2f028cc4f164eb2c5f3f6a18e268f35e01ed
SuDelk <sudul.fernando@gmail.com>
2025-11-06 10:42:58 +0530

    mongoDb connection added

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 
-
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// mongoDb connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("✅ MongoDB connected"))
+  .catch((err) => console.log("❌ DB error:", err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
